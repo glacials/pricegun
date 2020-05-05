447d06a6fa1553bd808a90f63b1170aa1c936e63
glacials <qhiiyr@gmail.com>
2020-05-05 16:17:33 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import Questionnaire from './Questionnaire.js'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      monthlyRate: 0,
+      monthlyCosts: 0,
+      costPerUser: 0,
+      pricePerUser: 0,
+    }
+
+    this.handleFormChange = this.handleFormChange.bind(this)
+  }
+
+  async handleFormChange(result) {
+    await this.setState({
+      monthlyRate: result.monthlyRate,
+      monthlyCosts: result.monthlyCosts,
+      costPerUser: result.costPerUser,
+      pricePerUser: result.pricePerUser,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Jumbotron>
+          <h1>Price My Product</h1>
+            <p>
+              Find out how much you should charge for your side project.
+              <br />
+              <i>No information leaves your computer.</i>
+            </p>
+        </Jumbotron>
+        <Container>
+          <Row>
+            <Col md={6}>
+              <Questionnaire onFormChange={this.handleFormChange} />
+            </Col>
+            <Col md={6} className="text-center">
+              <h5>You should charge users</h5>
+              <h1>${this.state.pricePerUser.toLocaleString(navigator.language, {minimumFractionDigits: 0, maximumFractionDigits: 0})} per month</h1>
+              <h5>Explanation:</h5>
+              Your rate: ${this.state.monthlyRate.toLocaleString(navigator.language, {minimumFractionDigits: 0, maximumFractionDigits: 0})} per month
+              <br />
+                Your project's costs: ${this.state.monthlyCosts.toLocaleString(navigator.language, {minimumFractionDigits: 0, maximumFractionDigits: 0})} per month
+              <br />
+                User cost: {this.state.costPerUser < 1 ?
+                    "< $1 " :
+                    `$${this.state.costPerUser.toLocaleString(navigator.language, {minimumFractionDigits: 0, maximumFractionDigits: 0})} `}
+                per user per month
+              <br />
+              <br />
+              <br />
+              <p>
+                <i>
+                  <small>
+                    This tool does not know whether users will pay this for the value they get.
+                    <br />
+                    That part is up to you. :)
+                  </small>
+                </i>
+              </p>
+              <div className="mx-auto my-5">
+                <small>
+                  <i>by Ben Carlsson @ <a href="https://twos.dev">twos.dev</a></i>
+                  <br />
+                  <i>Ship code faster with consistency across all teams @ <a href="https://yourbase.io">YourBase</a></i>
+                </small>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Questionnaire.js
@@ -0,0 +1,143 @@
+import React from 'react';
+
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+export default class Questionnaire extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hoursPerWeek: 10,
+      hostingDollarsPerMonthFixed: 100,
+      hostingDollarsPerMonthVariable: 100,
+      conversionRate: 0.5,
+      opportunityCostDollarsPerYear: 100000,
+      margin: 0.2,
+      numUsers: 1000,
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleChange({target: ''})
+  }
+
+  async handleChange(event) {
+    let val
+    switch (event.target.name) {
+      case 'margin':
+      val = Number(event.target.value) / 100
+      break
+      case 'conversionRate':
+      val = Number(event.target.value) / 100
+      break
+      default:
+      val = Number(event.target.value)
+    }
+
+    await this.setState({
+      [event.target.name]: val
+    })
+
+    const weeklyRate = (this.state.opportunityCostDollarsPerYear / 52) * (this.state.hoursPerWeek / 40)
+    const monthlyRate = (this.state.opportunityCostDollarsPerYear / 12) * (this.state.hoursPerWeek / 40)
+    const monthlyCosts = this.state.hostingDollarsPerMonthFixed + (this.state.hostingDollarsPerMonthVariable / 1000 * this.state.numUsers)
+    const costPerUser = (monthlyRate + monthlyCosts) / this.state.numUsers
+    const pricePerUser = costPerUser * (1 / this.state.conversionRate) * (1 + this.state.margin)
+
+    this.props.onFormChange({
+      monthlyRate: Math.floor(monthlyRate),
+      monthlyCosts: Math.floor(monthlyCosts),
+      costPerUser: Math.floor(costPerUser),
+      pricePerUser: Math.floor(pricePerUser),
+    })
+  }
+
+  render() {
+    return (
+      <Form>
+        <Form.Group>
+          <Form.Label>
+            How many hours per week do you work on it?
+          </Form.Label>
+          <Form.Control type="text" placeholder="10" name="hoursPerWeek" onChange={this.handleChange} autoFocus />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>
+            What are your <b>fixed</b> costs?
+          </Form.Label>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>$</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control type="text" placeholder="100" name="hostingDollarsPerMonthFixed" onChange={this.handleChange} />
+            <InputGroup.Append>
+              <InputGroup.Text>per month</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+          <Form.Text>i.e. how much would it cost you to stay online even with zero users?</Form.Text>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>
+            What are your <b>variable</b> costs, per 1k users?
+          </Form.Label>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>$</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control type="text" placeholder="100" name="hostingDollarsPerMonthVariable" onChange={this.handleChange} />
+            <InputGroup.Append>
+              <InputGroup.Text>per month</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>
+            What percentage of users will pay?
+          </Form.Label>
+          <InputGroup>
+            <Form.Control type="text" placeholder="1" name="conversionRate" onChange={this.handleChange}  />
+            <InputGroup.Append>
+              <InputGroup.Text>%</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>
+            How much do you make at your traditional job?
+          </Form.Label>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>$</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control type="text" placeholder="100000" name="opportunityCostDollarsPerYear" onChange={this.handleChange}  />
+            <InputGroup.Append>
+              <InputGroup.Text>per year</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>
+            What margins do you want?
+          </Form.Label>
+          <InputGroup>
+            <Form.Control type="text" placeholder="20" name="margin" onChange={this.handleChange}  />
+            <InputGroup.Append>
+              <InputGroup.Text>%</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>
+            How many users will you have?
+          </Form.Label>
+          <InputGroup>
+            <Form.Control type="text" placeholder="1000" name="numUsers" onChange={this.handleChange}  />
+            <InputGroup.Append>
+              <InputGroup.Text>users</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+          <Form.Text>Whether paying or non-paying.</Form.Text>
+        </Form.Group>
+      </Form>
+    )
+  }
+}
