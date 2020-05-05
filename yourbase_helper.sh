b96832ff8a14024c31130931e8d45ac407d39838
glacials <qhiiyr@gmail.com>
2020-05-05 16:55:01 -0700

    Fix subtree directory

--- yourbase_helper.sh
+++ yourbase_helper.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git subtree push --prefix dist https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git gh-pages
+git subtree push --prefix build https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git gh-pages
