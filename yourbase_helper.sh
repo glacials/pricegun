f3a8e8c7a78085445543840352b21be43a41fd25
glacials <qhiiyr@gmail.com>
2020-05-06 10:36:29 -0700

    Update subtree command

--- yourbase_helper.sh
+++ yourbase_helper.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-git push https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git `git subtree split --prefix build master`:gh-pages --force
+# To force
+# git push https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git `git subtree split --prefix build master`:gh-pages --force
+
+# Not to force
+git subtree push --prefix build origin gh-pages
