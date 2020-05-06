1db49315fe519bdbd3af49e097e3a65a15ddd821
glacials <qhiiyr@gmail.com>
2020-05-06 10:37:53 -0700

    Fix remote

--- yourbase_helper.sh
+++ yourbase_helper.sh
@@ -4,4 +4,4 @@
 # git push https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git `git subtree split --prefix build master`:gh-pages --force
 
 # Not to force
-git subtree push --prefix build origin gh-pages
+git subtree push --prefix build https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun gh-pages
