50a1552224143d89f6b1eca5f674def5dfff7635
glacials <qhiiyr@gmail.com>
2020-05-06 11:38:44 -0700

    Try a different deploy strat

--- yourbase_helper.sh
+++ yourbase_helper.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-# To force
+# Subtree - To force
 # git push https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git `git subtree split --prefix build master`:gh-pages --force
 
-# Not to force
-git subtree push --prefix build https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun gh-pages
+# Subtree - Not to force
+# git subtree push --prefix build https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun gh-pages
+
+mkdir -p build
+cd build
+git init
+git remote add origin https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun
+git pull --force
+cd ../
+yarn install
+yarn build
+cd build
+git add .
+git -c 'user.name=YourBase' -c 'user.email=ben@yourbase.io' commit -am 'Automatic deploy by YourBase'
+git push
+cd ../
