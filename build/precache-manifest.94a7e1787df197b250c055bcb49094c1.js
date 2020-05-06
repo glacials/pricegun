3cf28adcd2c88afcdecd6a1ef1bda918ddfd8b1b
glacials <qhiiyr@gmail.com>
2020-05-06 11:44:38 -0700

    Automatic deploy test

---
+++ build/precache-manifest.94a7e1787df197b250c055bcb49094c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34f5c1b31edaf3497f17c01cf01d6a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c96a0264ea4ccc86ed3c",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "8aee5b0f9653b6c231e9",
+    "url": "/static/css/main.f3520556.chunk.css"
+  },
+  {
+    "revision": "c96a0264ea4ccc86ed3c",
+    "url": "/static/js/2.a7270855.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a7270855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aee5b0f9653b6c231e9",
+    "url": "/static/js/main.63e26cb3.chunk.js"
+  },
+  {
+    "revision": "253116d0d10df9f96ed1",
+    "url": "/static/js/runtime-main.02888262.js"
+  }
+]);
