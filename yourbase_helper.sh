5d7100b753b42e051263e1428809d44e00969a64
glacials <qhiiyr@gmail.com>
2020-05-05 19:35:39 -0700

    Swap deploy solutions again

--- yourbase_helper.sh
+++ yourbase_helper.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Old solution -- can't force push
-#git subtree push --prefix build https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git gh-pages
+git push origin `git subtree split --prefix build master`:gh-pages --force
 
-cd build
-git init
-git add .
-git -c "user.name=YourBase" -c "user.email=ben@yourbase.io" commit -am "Automatic build by YourBase" --allow-empty
-git push --force https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git gh-pages
-rm -rf .git
-cd ..
+# Other solution
+# cd build
+# git init
+# git add .
+# git -c "user.name=YourBase" -c "user.email=ben@yourbase.io" commit -am "Automatic build by YourBase" --allow-empty
+# git push --force https://glacials:${GITHUB_TOKEN}@github.com/glacials/pricegun.git gh-pages
+# rm -rf .git
+# cd ..
