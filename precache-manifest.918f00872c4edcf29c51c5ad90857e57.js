42fdad6f37b5e8406ac3e3e25a73035acea4aad2
YourBase <ben@yourbase.io>
2020-12-12 17:46:57 +0000

    Automatic deploy by YourBase

---
+++ precache-manifest.918f00872c4edcf29c51c5ad90857e57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1523d05381a11568a4c2fcdc378b6538",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3ac739366c00dec4eeb",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e3812b158560d34adb19",
+    "url": "/static/css/main.f3520556.chunk.css"
+  },
+  {
+    "revision": "c3ac739366c00dec4eeb",
+    "url": "/static/js/2.49eb3eae.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/static/js/2.49eb3eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3812b158560d34adb19",
+    "url": "/static/js/main.91bbb8d8.chunk.js"
+  },
+  {
+    "revision": "253116d0d10df9f96ed1",
+    "url": "/static/js/runtime-main.02888262.js"
+  }
+]);
